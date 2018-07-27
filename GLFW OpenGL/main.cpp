5b468b73871b35849323a9d36b000989e3b84dc9
Space <agphysics@live.com>
2018-07-27 13:33:23 +1000

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by SpaceMan on 27/7/18.
+//  Copyright © 2018 Space. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
