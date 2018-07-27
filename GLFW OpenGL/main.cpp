337c42260f95a8eda43d2bca7bec1a0301895ac2
Space <agphysics@live.com>
2018-07-27 15:32:03 +1000

    Modern OpenGL+ [Setup] GLFW and GLEW on a Mac https://youtu.be/Tz0dq2krCW8

--- GLFW OpenGL/main.cpp
+++ GLFW OpenGL/main.cpp
@@ -1,15 +1,57 @@
-//
-//  main.cpp
-//  GLFW OpenGL
-//
-//  Created by SpaceMan on 27/7/18.
-//  Copyright © 2018 Space. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//GLFW3
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //need for mac
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+    
+    int screenWidth, screenHeight; //need for mac with retina display
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight); //need for mac with retina display
+    
+    if (nullptr == window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    
+    if(GLEW_OK != glewInit())
+    {
+        std::cout <<"Failed to initialise GLEW" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    
+    }
+    
+    glfwTerminate();
 }
